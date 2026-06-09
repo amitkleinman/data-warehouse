1851a7655a53bc49592c580353d8ef8cd5d7490c
amitkleinman <96815016+amitkleinman@users.noreply.github.com>
2026-06-09 12:45:34 +0300

    Update init.sql

--- scripts/init.sql
+++ scripts/init.sql
@@ -8,3 +8,8 @@ Script creates new datawarehouse
 
 /*
 
+create database if not exists DatawarehouseDATAWAREHOUSE;
+
+create schema if not exists bronze;
+create schema if not exists silver;
+create schema if not exists bronze;
